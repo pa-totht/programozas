715406c9c68501280a602aeda24145ae2946d35a
pa-totht <112852328+pa-totht@users.noreply.github.com>
2022-10-10 08:34:34 +0200

    Add files via upload

---
+++ 9-10.ora hf.py
@@ -0,0 +1,11 @@
+a=int(input("kérek egy számot: "))
+b=int(input("kérek még egy számot: "))
+c=input("kérek egy müveletett: ")
+if  c=="+":
+    print(a+b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+elif c=="-":
+    print(a-b)
