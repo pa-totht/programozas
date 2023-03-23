99456aa179c34888c26e53e3ca6ac94445f5eead
pa-totht <112852328+pa-totht@users.noreply.github.com>
2023-03-23 07:58:02 +0100

    Add files via upload

---
+++ fuggveny_01.py
@@ -0,0 +1,25 @@
+x = ord("h")
+#A ord()függvény egy megadott karakter unicode kódját képviselő számot adja vissza.
+
+x = chr(97)
+#A chr()függvény a megadott unicode-ot képviselő karaktert adja vissza.
+
+x = min(5, 10)
+#A min()függvény a legalacsonyabb értékű elemet, vagy a legalacsonyabb értékű elemet adja vissza az iterációban.
+
+#Ha az értékek karakterláncok, akkor ábécé szerinti összehasonlítás történik.
+
+x = max(5, 10)
+#A max()függvény a legmagasabb értékű elemet, vagy a legmagasabb értékű elemet adja vissza az iterálható elemben.
+
+#Ha az értékek karakterláncok, akkor ábécé szerinti összehasonlítás történik.
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry")
+#A index()metódus a megadott érték első előfordulásakor adja vissza a pozíciót.
+
+mylist = ["apple", "banana", "cherry"]
+#A listák több elem tárolására szolgálnak egyetlen változóban.
+
+
